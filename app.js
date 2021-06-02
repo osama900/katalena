3f3c013aa161058b71e907ebecebf87bcca4d3df
osamation <>
2021-06-02 13:38:49 +0000

    js code added

---
+++ app.js
@@ -0,0 +1,16 @@
+var welcom = "H! to you in KATALENA"
+alert (welcom)
+
+var name =prompt("What is your name ?")
+console.log(name)
+alert ("nice to see you" + name)
+var age = prompt("How old are you ?")
+console.log(age)
+
+if(age <10){
+  document.write("hellow kid")
+}
+else if(age >=10 && age <30){
+  document.write("hi man")
+}
+
