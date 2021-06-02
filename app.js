ae88b169c08759559637fa935de1b30d714088e0
osamation <>
2021-06-02 14:00:32 +0000

    enhance if

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ else if(age >=10 && age <30){
   document.write("<div>"+"sdfgfddfgfd"+"</div>")
 }
 else if(age >= 30 && age < 50) {
-  document.write("<h3> "+ "hellow mad"+"</h3>")
+  document.write("<div> "+ "hellow man"+"</div>")
+}
+else{
+    document.write("<div> "+ "hellow sair"+"</div>")
+
 }
 
