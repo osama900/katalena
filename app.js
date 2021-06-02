c305d2839c45282f4caa640024b3d0c28396520f
osamation <>
2021-06-02 13:53:53 +0000

    js code add

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var welcom = "H! to you in KATALENA"
-alert (welcom)
+alert(welcom)
 
-var name =prompt("What is your name ?")
+var name = prompt("What is your name ?")
 console.log(name)
-alert ("nice to see you" + name)
+alert("nice to see you" + name)
 var age = prompt("How old are you ?")
 console.log(age)
 
-if(age <10){
-  document.write("hellow kid")
+if (age < 10) {
+  document.write("<h3> "+ "hellow kid"+"</h3>")
 }
 else if(age >=10 && age <30){
-  document.write("hi man")
+  document.write("<div>"+"sdfgfddfgfd"+"</div>")
+}
+else if(age >= 30 && age < 50) {
+  document.write("<h3> "+ "hellow mad"+"</h3>")
 }
 
